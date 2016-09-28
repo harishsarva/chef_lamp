a90de37df5e53d0d3d2aa91d0854f7da4c6011f4
hsarva <harish.sarva@genesys.com>
2016-09-28 15:39:38 -0400

    edit the default.rb file

--- cookbooks/apache_php/recipes/default.rb
+++ cookbooks/apache_php/recipes/default.rb
@@ -2,6 +2,7 @@
 # Cookbook Name:: apache_php
 # Recipe:: default
 #
+#My Name is harish Sarva And I made this changes on purpose.
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'selinux::permissive'
